8dcdd0d809e60687850cf1ba6ce26acaa9b57b89
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-14 17:40:54 -0300

    Test: Testando commit automatizando de novo

--- pull-server.sh
+++ pull-server.sh
@@ -9,7 +9,7 @@ git commit -am "$message"
 git push server main
 git push origin main
 
-ssh lucas@http://35.198.22.191/ \
+ssh lucas@35.198.22.191 \
   'git -C /home/lucas/api/ ' \
   'pull origin main && ' \
   'pm2 restart api && sudo systemctl restart nginx'
