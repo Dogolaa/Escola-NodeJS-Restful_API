b45732967df37930195e9132a719fd4efbc327b1
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-17 19:30:41 -0300

    fix: pull

--- pull-server.sh
+++ pull-server.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
 npm run build
 git add .
 
-read message
-echo "Mensagem do commit: "
+read -p "Digite a mensagem do commit: " message
 git commit -am "$message"
-git push server main
+
 git push origin main
 
-ssh lucas@35.198.22.191 \
-  'git -C /home/lucas/api/ ' \
-  'pull origin main && ' \
-  'pm2 restart api && sudo systemctl restart nginx'
+ssh lucas@35.198.22.191 << EOF
+cd /home/lucas/api
+git pull origin main
+pm2 restart api
+sudo systemctl restart nginx
+EOF
