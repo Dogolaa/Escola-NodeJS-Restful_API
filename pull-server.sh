435e1ebf22379e034ca8d5628c7581d5f22a6985
Lucas Dogo <lucasdogo01@gmail.com>
2025-02-17 19:33:04 -0300

    fix

--- pull-server.sh
+++ pull-server.sh
@@ -6,12 +6,18 @@ git add .
 read -p "Digite a mensagem do commit: " message
 git commit -am "$message"
 
+# Envia as mudanças para o servidor
 git push origin main
-git push server main  # Se der erro, tente manualmente para ver o problema
+git push server main
 
+# Atualiza o repositório no servidor e a API
 ssh lucas@35.198.22.191 << EOF
+# Atualiza o repo-api primeiro
+cd /home/lucas/repo-api
+git pull origin main
+
+# Atualiza o api
 cd /home/lucas/api
-git reset --hard origin/main  # Evita conflitos no pull
 git pull origin main
 pm2 restart api
 sudo systemctl restart nginx
